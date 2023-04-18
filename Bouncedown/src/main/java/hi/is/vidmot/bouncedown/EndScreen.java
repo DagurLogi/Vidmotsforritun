655bf7e22f984c271bf4a2b345152121ed41c75f
DagurLogi <dagurlogo@gmail.com>
2023-04-18 00:06:47 +0000

    - Main menu og End menu komið
    Main Menu og End menu eru tilbúið.
    Vantar að láta exit takkana virka og láta leikinn runna þegar ýtt er á spila (Main menu) eða Reyna aftur (end menu).
    Einnig vantar að gera high score-ið líka

---
+++ Bouncedown/src/main/java/hi/is/vidmot/bouncedown/EndScreen.java
@@ -0,0 +1,16 @@
+package hi.is.vidmot.bouncedown;
+
+public class EndScreen {
+
+    public void onMain() {
+        ViewSwitcher.switchTo(View.MainMenu);
+    }
+
+    public void onAgain() {
+        ViewSwitcher.switchTo(View.Leikur);
+    }
+
+    public void onExit() {
+
+    }
+}
