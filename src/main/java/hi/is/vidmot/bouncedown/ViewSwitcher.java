b3ab5d83d4d42a1fdf95cc3d0cfdb5b9efd207e2
DagurLogiO <dagurlogo@gmail.com>
2023-04-18 00:27:00 +0000

    Buinn að laga error

---
+++ src/main/java/hi/is/vidmot/bouncedown/ViewSwitcher.java
@@ -0,0 +1,34 @@
+package hi.is.vidmot.bouncedown;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class ViewSwitcher {
+
+    private static Scene scene;
+
+    public static void setScene(Scene scene) {
+        ViewSwitcher.scene = scene;
+    }
+
+    public static void switchTo(View view){
+        if (scene == null) {
+            System.out.println("No scene was set");
+            return;
+        }
+
+        try {
+            Object root = FXMLLoader.load(Objects.requireNonNull(ViewSwitcher.class.getResource(view.getFileName())));
+            scene.setRoot((Parent) root);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+}
---
+++ src/main/java/hi/is/vidmot/bouncedown/mainMenu.java
@@ -0,0 +1,30 @@
+package hi.is.vidmot.bouncedown;
+
+import javafx.scene.control.Button;
+
+import static javafx.application.Application.launch;
+
+public class mainMenu {
+    public Button StartGame;
+    public Button HighScore;
+    public Button Exitbutton;
+
+    public void onStart() {
+        ViewSwitcher.switchTo(View.Leikur);
+    }
+    public void onHighScore() {
+        ViewSwitcher.switchTo(View.EndScreen);
+    }
+
+    public void onEnd() {
+
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+    }
+
+
+
+
+
